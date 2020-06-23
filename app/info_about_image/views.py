8deb5fa38eba7d723421ce6c2848e59b86d588fe
strange-capp <andriy.portman@gmail.com>
2020-06-23 14:08:58 +0300

    add delete cacged images

--- app/info_about_image/views.py
+++ app/info_about_image/views.py
@@ -4,8 +4,26 @@ from werkzeug.utils import secure_filename
 from ..for_dev.all_exts import all_to
 import os
 from PIL import Image
+from .. import scheduler
+import time
 
 suitable = ["." + ext.lower() for ext in all_to]
+
+
+def delete_file(*filenames):
+    """
+    This function remove all files in 'filenames'
+    :param filenames:
+    :return: None
+    """
+    # sleep
+    time.sleep(1800)
+
+
+    # then delete all files
+    for filename in filenames:
+        os.remove(os.getcwd()+'/app/static/users_images/'+filename)
+        print(os.getcwd()+'/app/static/users_images/'+filename, 'is deleted')
 
 
 @info_about_image.route('/', methods=['GET'])
@@ -42,6 +60,8 @@ def show_info():
     height = image.height
     image_format = image.format
     size = os.path.getsize(filepath)
+
+    scheduler.add_job(func=delete_file, trigger='date', args=[filename], id=filename)
     
 
     return render_template('info.html', name=name, mode=mode,
